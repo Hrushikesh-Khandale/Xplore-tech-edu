cb23fee28d64cb160e5c52fe9fa093fcef144324
Hrushikesh <khandalehrushikesh@gmail.com>
2023-11-30 21:21:58 +0530

    improved code index file

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,27 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const express=require('express');
+const router = express.Router();
+const routes = require('./routes/landing-page');
 
-
-const mongoDB = 'mongodb://localhost:27017/my-landing-page-database';
+const mongoDB = 'mongodb://localhost:27017/landing-page-database';
 const connectionOptions = {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 };
 
-
-mongoose.connect(mongoDB, connectionOptions, () => {
+mongoose.connect(mongoDB, connectionOptions, (err) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+  } else {
     console.log('Connected to MongoDB');
-  });
-  
+  }
+});
+
+
+
+router.use('/landing-page', routes);
+
+
+router.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
