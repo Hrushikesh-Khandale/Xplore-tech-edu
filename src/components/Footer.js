ec245c9c915be721afcb33907e29afa689ba8ff9
Hrushikesh <khandalehrushikesh@gmail.com>
2023-12-25 19:13:37 +0530

    Footer component added

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 Your Company Name</p>
+      <ul className="footer-links">
+        <li>
+          <a href="/">About Us</a>
+        </li>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Contact Us</a>
+        </li>
+        <li>
+          <a href="/">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="/">Terms of Use</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
