2eb303e4477774fb6041e0c0110e37efaa6b4d89
Hrushikesh <khandalehrushikesh@gmail.com>
2023-12-26 21:58:50 +0530

    Added dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,31 +2,26 @@ import React, { useState } from "react";
 import "../App.css";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 import Logo from "./Logo";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Navbar = () => {
-  // State to manage the collapse/expand of the menu
   const [isCollapsed, setCollapsed] = useState(false);
 
-  // Function to toggle the collapse state
   const toggleCollapse = () => {
     setCollapsed(!isCollapsed);
   };
 
   return (
     <>
-      {/* Navbar Header */}
       <header className="navBar">
-        {/* Logo */}
-        <Logo />
-
-        {/* Menu Toggle Button */}
+        <section>
+          <Logo />
+        </section>
         <div className="menu-toggle" onClick={toggleCollapse}>
-          {/* Hamburger Menu Bars */}
           <div className={`bar ${isCollapsed ? "collapsed" : ""}`}></div>
           <div className={`bar ${isCollapsed ? "collapsed" : ""}`}></div>
           <div className={`bar ${isCollapsed ? "collapsed" : ""}`}></div>
         </div>
-
         <ul className={`navLists ${isCollapsed ? "collapsed" : ""}`}>
           <Link className="link" to="/homepage">
             Home
@@ -34,6 +29,24 @@ const Navbar = () => {
           <Link className="link" to="/about">
             About Us
           </Link>
+          <li
+            className="link"
+            style={{
+              color: "white",
+              textDecoration: "none",
+              width: "fit-content",
+            }}
+          >
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Dropdown"
+              menuVariant="dark"
+              className="dropDown"
+            >
+              <NavDropdown.Item className="dropdnOptions"  href="/">Tutorials</NavDropdown.Item>
+              <NavDropdown.Item className="dropdnOptions" href="/">Feedback</NavDropdown.Item>
+            </NavDropdown>
+          </li>
         </ul>
       </header>
     </>
