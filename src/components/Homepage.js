aa68974358d6a7a1c59cba5e6803075ff0965491
Hrushikesh <khandalehrushikesh@gmail.com>
2023-12-27 16:15:22 +0530

    Homepage UI changed

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
+import logo from "./logo.png";
 
 const Homepage = () => {
   return (
     <>
-    <div>
-        <h1>Xplore - </h1>
-    </div>
-    </>
-  )
-}
+      <div className="homepage">
+        {/* Logo and button/link */}
+        <p className="p1">X</p>
+        <p className="p2">Xplore</p>
+        <p className="p3">Learn from Anywhere</p>
 
-export default Homepage
+        <a
+          className="learnMore"
+          href="https://www.coders.dev/blog/emerging-technologies-and-trends-to-watch.html"
+          target="_blank" rel="noreferrer"
+        >
+          Learn More
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
