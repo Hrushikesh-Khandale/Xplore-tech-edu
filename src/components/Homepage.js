6cb9a28510870558092e290981803369bd313788
Hrushikesh <khandalehrushikesh@gmail.com>
2023-12-27 16:37:12 +0530

    Added arrow animation and link button to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -13,10 +13,35 @@ const Homepage = () => {
         <a
           className="learnMore"
           href="https://www.coders.dev/blog/emerging-technologies-and-trends-to-watch.html"
-          target="_blank" rel="noreferrer"
+          target="_blank"
+          rel="noreferrer"
         >
           Learn More
         </a>
+
+        {/* arrow svg */}
+        <div className="downArrow">
+          <svg
+            data-slot="icon"
+            fill="none"
+            stroke-width="1.5"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            ></path>
+          </svg>
+        </div>
+
+
+        <div className="services">
+            
+        </div>
       </div>
     </>
   );
