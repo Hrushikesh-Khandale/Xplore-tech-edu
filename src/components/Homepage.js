fb13d77a93dbeea24b390413340bd7700362f8b0
Hrushikesh <khandalehrushikesh@gmail.com>
2023-12-28 18:35:44 +0530

    Updated styles in App.css. Add programming language icons and links in Homepage.js.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,9 @@
 import React from "react";
-import logo from "./logo.png";
+import HTML from "./LOGOS/html.png";
+import CSS from "./LOGOS/css.png";
+import JS from "./LOGOS/js.png";
+import NODEJS from "./LOGOS/nodejs.png";
+import REACTJS from "./LOGOS/reactjs.png";
 
 const Homepage = () => {
   return (
@@ -37,10 +41,68 @@ const Homepage = () => {
             ></path>
           </svg>
         </div>
+      </div>
+      <br />
+      {/* Proframming languages */}
+      <div className="languageLinks">
+        <h1 className="languageHeader">Programming Languages</h1>
+        <div className="container">
+          {/* html */}
+          <button className="html">
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={HTML} alt="HTML" />
+            </a>
+            <b><p>HTML</p></b>
+          </button>
+          {/* css */}
+          <button className="css">
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={CSS} alt="CSS" />
+            </a>
+            <b><p>Javascript</p></b>
+          </button>
+          {/* JS */}
+          <button className="javascript">
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={JS} alt="Javascript" />
+            </a>
+            <b><p>CSS</p></b>
+          </button>
+          {/* Nodejs */}
 
-
-        <div className="services">
-            
+          <button className="nodejs">
+            <a
+              href="https://nodejs.org/en/learn/getting-started/introduction-to-nodejs"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={NODEJS} alt="NodeJS" />
+            </a>
+            <b><p>NodeJS</p></b>
+          </button>
+          {/* Reactjs */}
+          <button className="reactjs">
+            <a
+              href="https://react.dev/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={REACTJS} alt="ReactJS" />
+            </a>
+            <b><p>ReactJS</p></b>
+          </button>
         </div>
       </div>
     </>
