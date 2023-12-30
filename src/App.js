e7c0f0733ddb37b2cb9743aee2feca2ecbdab7e3
Hrushikesh <khandalehrushikesh@gmail.com>
2023-12-30 16:46:17 +0530

    Update CSS of App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-// App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from "./components/Footer";
+import Footers from "./components/Footers";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
 
 const App = () => {
-
-
-
   return (
     <Router>
       <Navbar />
       <Homepage />
       <Switch>
-        <Route exact path="/" component={Footer} />
+        <Route exact path="/" component={Footers} />
       </Switch>
     </Router>
   );
