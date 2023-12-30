29a26ab219256effc779899317fb291727bc8ef2
Hrushikesh <khandalehrushikesh@gmail.com>
2023-12-30 16:46:49 +0530

    Added more content to Description.js

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,118 +1,131 @@
 import React from "react";
 import "../App.css";
-import Carousal from '../components/Carousal';
-
+import Carousal from "../components/Carousal";
 
 const Description = () => {
   return (
     <>
-      <h1 className="services">Services</h1>
       <div className="description">
-        {/* Learning Hub */}
-        <div className="card">
-          <span>
-            <svg
-              data-slot="icon"
-              fill="none"
-              stroke-width="1.5"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
-              ></path>
-            </svg>
-          </span>
-          <h2>Learning Hub</h2>
-          <span>
-            providing resources for individuals interested in programming
-            languages.
-          </span>
-        </div>
+        <h2
+          className="services"
+          style={{
+            color: " #665",
+            backgroundColor: "white",
+            fontWeight: "500",
+            textAlign: "center",
+            position: "relative",
+            left: "45%",
+            he: "fit-content",
+          }}
+        >
+          Services
+        </h2>
+          {/* Learning Hub */}
+          <div className="cards">
+            <span className="service">
+              <svg
+                data-slot="icon"
+                fill="none"
+                stroke-width="1.5"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+                ></path>
+              </svg>
+            </span>
+            <h2>Learning Hub</h2>
+            <span className="cardTexts">
+              providing resources for individuals interested in programming
+              languages.
+            </span>
+          </div>
 
-        {/* Diverse Content */}
-        <div className="card">
-          <span>
-            <svg
-              data-slot="icon"
-              fill="none"
-              stroke-width="1.5"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
-              ></path>
-            </svg>
-          </span>
-          <h2>Diverse Content</h2>
-          <span>
-            {" "}
-            This ensures a diverse set of resources to cater to different
-            learning preferences.
-          </span>
-        </div>
+          {/* Diverse Content */}
+          <div className="cards">
+            <span className="service">
+              <svg
+                data-slot="icon"
+                fill="none"
+                stroke-width="1.5"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+                ></path>
+              </svg>
+            </span>
+            <h2>Diverse Content</h2>
+            <span className="cardTexts">
+              {" "}
+              This ensures a diverse set of resources to cater to different
+              learning preferences.
+            </span>
+          </div>
 
-        {/* User Friendly Interface */}
-        <div className="card">
-          <span>
-            <svg
-              data-slot="icon"
-              fill="none"
-              stroke-width="1.5"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z"
-              ></path>
-            </svg>
-          </span>
-          <h2>User Friendly Interface</h2>
-          <span>
-            The web app features a user-friendly interface with a visually
-            appealing design.
-          </span>
-        </div>
+          {/* User Friendly Interface */}
+          <div className="cards">
+            <span className="service">
+              <svg
+                data-slot="icon"
+                fill="none"
+                stroke-width="1.5"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z"
+                ></path>
+              </svg>
+            </span>
+            <h2>User Friendly Interface</h2>
+            <span className="cardTexts">
+              The web app features a user-friendly interface with a visually
+              appealing design.
+            </span>
+          </div>
 
-        {/* Encouraging Exploration */}
-        <div className="card">
-          <span>
-            <svg
-              data-slot="icon"
-              fill="none"
-              stroke-width="1.5"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              ></path>
-            </svg>
-          </span>
-          <h2> Encouraging Exploration</h2>
-          <span>
-            Encourages users to explore emerging technologies and trends exploration beyond the basics.
-          </span>
-        </div>
+          {/* Encouraging Exploration */}
+          <div className="cards">
+            <span className="service">
+              <svg
+                data-slot="icon"
+                fill="none"
+                stroke-width="1.5"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                ></path>
+              </svg>
+            </span>
+            <h2> Encouraging Exploration</h2>
+            <span className="cardTexts">
+              Encourages users to explore emerging technologies and trends
+              exploration beyond the basics.
+            </span>
+          </div>
       </div>
-      <Carousal/>
+      <Carousal />
     </>
   );
 };
